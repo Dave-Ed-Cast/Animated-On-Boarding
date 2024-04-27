7dc708b05a436d7742c20ff5162025e629928c39
Dave-Ed-Cast <dcastaldi23@fed.idserve.net>
2024-04-27 13:25:10 +0200

    Quick fix of folder placement

---
+++ LottieOnBoarding/LottieOnBoardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottieOnBoardingApp.swift
+//  LottieOnBoarding
+//
+//  Created by Davide Castaldi on 27/04/24.
+//
+
+import SwiftUI
+
+@main
+struct LottieOnBoardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
